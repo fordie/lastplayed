0d5a1b0c058c1acce243016d88247ad94a547290
Mark Ford <mark@Mintelligentli.me>
2016-09-21 12:13:40 +0100

    add keys file

---
+++ modules/apikeys.js
@@ -0,0 +1,12 @@
+/*
+* @Author: Mark Ford
+* @Date:   2016-09-21 08:22:06
+* @Last Modified by:   Mark Ford
+* @Last Modified time: 2016-09-21 12:13:12
+*/
+
+'use strict';
+
+module.exports ={
+	lastFmApiKey : 'your_api_key'
+};
